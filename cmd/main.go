e782fb1dfefe72a95cbcfc3ea373dd0591a1bfb9
Aslon <hamidovaslon1@gmail.com>
2025-06-23 07:45:41 +0500

    transactions for suppliers, sales

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,20 @@ package main
 
 import "aslon1213/magazin_pos/pkg/app"
 
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
 func main() {
 
 	app := app.NewApp()
--- pkg/repository/output.go
+++ pkg/repository/output.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/gofiber/fiber/v2"
+
 type Error struct {
 	Message string `json:"message"`
 	Code    int    `json:"code"`
@@ -10,9 +12,26 @@ type Output struct {
 	Error []Error     `json:"error"`
 }
 
-func NewOutput(data interface{}, errors ...Error) Output {
-	return Output{
-		Data:  data,
-		Error: errors,
+func NewOutput(data interface{}, errors ...Error) map[string]interface{} {
+	return map[string]interface{}{
+		"data":  data,
+		"error": errors,
 	}
+}
+
+func NewError(message string, code int) Error {
+	return Error{
+		Message: message,
+		Code:    code,
+	}
+}
+func NewErrors(errors ...error) []Error {
+	errs := []Error{}
+	for _, err := range errors {
+		errs = append(errs, Error{
+			Message: err.Error(),
+			Code:    fiber.StatusInternalServerError,
+		})
+	}
+	return errs
 }
